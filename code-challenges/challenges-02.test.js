28dffc6ba32a0705239ed528c321cdc42d1c3947
fati-ma <fatima.atiyya@yahoo.com>
2020-11-17 22:54:53 +0200

    challenge-03 done

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -10,6 +10,11 @@ Write a function named raisedToTheThird that takes in an array of numbers and re
 
 const raisedToTheThird = (arr) => {
   // Solution code here...
+  let newArr = [];
+  arr.forEach((val)=>{
+    newArr.push(Math.pow(val, 3))
+  });
+  return newArr;
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -21,6 +26,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  let str2 = str+' The end.';
+  return str2;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -38,6 +45,8 @@ console.log(a) prints [1, 2, 3, 1]
 
 const appendFirstToLast = (arr) => {
   // Solution code here...
+    arr.push(arr[0]);
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -57,6 +66,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -74,6 +84,9 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  people.forEach(val =>{
+    val.isAuthor = true;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -93,6 +106,10 @@ console.log(a) prints [1, 2, 3, 4]
 
 const append = (arr1, arr2) => {
   // Solution code here...
+  arr2.forEach(val => {
+    arr1.push(val);
+  });
+
 
 };
 
@@ -141,7 +158,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
@@ -152,7 +169,7 @@ xdescribe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
